d944a045c973f0766361bd5c1645170397d855be
aadesh73132 <145355002+aadesh73132@users.noreply.github.com>
2023-09-29 19:59:02 +0530

    Add files via upload

---
+++ day11.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int number,square;
+
+    printf("enter the number\n");
+    scanf("%d",&number);
+    square=number*number;
+    printf("you have enter the square as %d\n",square);
+
+    return 0;
+}
